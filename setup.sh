7469350c7367034d9590cf2d6da5b4e5a56818ab
Michael Brenner <mab7470@rit.edu>
2026-02-19 17:28:32 -0500

    added setup.sh and created nextjs + tailwind frontend folder

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+STORE_PATH="/opt/comp/store"
+
+if [ ! -f "backend/env/.internal.env" ] ; then
+    printf "Missing .internal.env in backend/env. Generating secure password and JWT secret...\n"
+
+    postgres_pass=$(openssl rand -base64 24 | tr -dc 'A-Za-z0-9' | head -c 24)
+
+    touch .internal.env
+    set -o noclobber
+    echo "POSTGRES_PASSWORD=$postgres_pass" >> .internal.env
+    printf "\n.internal.env file created and populated!\n"
+    printf "You can change these values manually by going into the .internal.env file 
+manually and editing the file"
+fi
+
+
+printf "\nCompleted setup!\n"
+
