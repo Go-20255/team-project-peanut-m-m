e24f108135ff11e99530d9df3590110c9a1bbda4
0xmmalik <manavmalik36@gmail.com>
2026-03-23 15:46:32 -0400

    fixed game state and style

--- backend/internal/db/game_state/game_state_db.go
+++ backend/internal/db/game_state/game_state_db.go
@@ -7,18 +7,16 @@ import (
 	"github.com/rs/zerolog"
 )
 
+func GameStateExists(log zerolog.Logger, ctx context.Context, tx *pgxpool.Tx, sessionId string) error {
 
-func GameStateExists(log zerolog.Logger, ctx context.Context, tx *pgxpool.Tx, session_id int) error {
-
-    rows, err := tx.Query(ctx, `
+	rows, err := tx.Query(ctx, `
         SELECT session_id FROM Game_State WHERE session_id = $1
-        `, session_id)
-    if err != nil{
-        // session id does not exist
-        rows.Close()
-        return err
-    }
-    rows.Close()
-    return nil
+        `, sessionId)
+	if err != nil {
+		// session id does not exist
+		rows.Close()
+		return err
+	}
+	rows.Close()
+	return nil
 }
-
--- backend/internal/db/players/players_db.go
+++ backend/internal/db/players/players_db.go
@@ -7,16 +7,15 @@ import (
 	"github.com/rs/zerolog"
 )
 
+func CreatePlayerDB(log zerolog.Logger, ctx context.Context, tx *pgxpool.Tx, name string, session_id string) error {
 
-func CreatePlayerDB(log zerolog.Logger, ctx context.Context, tx *pgxpool.Tx, name string, session_id int) error {
-
-    _, err := tx.Exec(ctx, `
+	_, err := tx.Exec(ctx, `
         INSERT INTO Player (name, session_id)
         VALUES ($1, $2)
         `, name, session_id)
-    if err != nil {
-        log.Trace().Err(err).Msgf("failed to insert player %v into db with session id %v", name, session_id)
-        return err
-    }
-    return nil
+	if err != nil {
+		log.Trace().Err(err).Msgf("failed to insert player %v into db with session id %v", name, session_id)
+		return err
+	}
+	return nil
 }
