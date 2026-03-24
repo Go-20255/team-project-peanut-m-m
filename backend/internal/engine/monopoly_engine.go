d222bcaff0a6ba6ef3495e4ae10a9aaf36459f9b
0xmmalik <manavmalik36@gmail.com>
2026-03-24 15:58:19 -0400

    fixed indents

--- backend/internal/engine/monopoly_engine.go
+++ backend/internal/engine/monopoly_engine.go
@@ -1,94 +1,94 @@
 package monopoly_engine
 
 import (
-	"context"
-	"fmt"
-	"monopoly-backend/handlers"
-	internaldb "monopoly-backend/internal/db"
-	"sync"
+    "context"
+    "fmt"
+    "monopoly-backend/handlers"
+    internaldb "monopoly-backend/internal/db"
+    "sync"
 
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+    "github.com/jackc/pgx/v5/pgxpool"
+    "github.com/rs/zerolog"
+    "github.com/rs/zerolog/log"
 )
 
 var (
-	engineManager   = map[string]*MonopolyEngine{}
-	engineManagerMu sync.Mutex
+    engineManager   = map[string]*MonopolyEngine{}
+    engineManagerMu sync.Mutex
 )
 
 type UserActionEvent struct {
-	Event string
-	Data  string
+    Event string
+    Data  string
 }
 
 func GetEngineBroker(sessionId string) (*handlers.SseBroker, error) {
-	engine := engineManager[sessionId]
-	if engine == nil {
-		return nil, fmt.Errorf("engine is nil for provided session_id")
-	}
-	broker := engine.broker
-	if broker == nil {
-		return nil, fmt.Errorf("engine exists but broker is nil. HUHH?")
-	}
-	return broker, nil
+    engine := engineManager[sessionId]
+    if engine == nil {
+        return nil, fmt.Errorf("engine is nil for provided session_id")
+    }
+    broker := engine.broker
+    if broker == nil {
+        return nil, fmt.Errorf("engine exists but broker is nil. HUHH?")
+    }
+    return broker, nil
 }
 
 func NotifyEngineOfAction(sessionId string, actionEvent UserActionEvent) error {
-	engine, ok := engineManager[sessionId]
-	if !ok {
-		return fmt.Errorf("engine does not exist for provided session_id at this point in time")
-	}
+    engine, ok := engineManager[sessionId]
+    if !ok {
+        return fmt.Errorf("engine does not exist for provided session_id at this point in time")
+    }
 
-	engine.userActionsChan <- actionEvent
+    engine.userActionsChan <- actionEvent
 
-	return nil
+    return nil
 }
 
 type MonopolyEngine struct {
-	sessionId         string
-	broker            *handlers.SseBroker
-	userActionsChan   chan UserActionEvent // using SseEvent as a template for now
-	userActionsChanMu sync.Mutex
+    sessionId         string
+    broker            *handlers.SseBroker
+    userActionsChan   chan UserActionEvent // using SseEvent as a template for now
+    userActionsChanMu sync.Mutex
 }
 
 func SetupNewMonopolyEngine(sessionId string) {
-	log := log.Logger.With().Str("session_id", sessionId).Logger()
+    log := log.Logger.With().Str("session_id", sessionId).Logger()
 
-	engine := MonopolyEngine{
-		sessionId:       sessionId,
-		broker:          handlers.NewSseBroker(),
-		userActionsChan: make(chan UserActionEvent),
-	}
+    engine := MonopolyEngine{
+        sessionId:       sessionId,
+        broker:          handlers.NewSseBroker(),
+        userActionsChan: make(chan UserActionEvent),
+    }
 
-	engineManagerMu.Lock()
-	engineManager[sessionId] = &engine
-	engineManagerMu.Unlock()
+    engineManagerMu.Lock()
+    engineManager[sessionId] = &engine
+    engineManagerMu.Unlock()
 
-	ctx := context.Background()
+    ctx := context.Background()
 
-	// infinite loop so we can recover from errors
-	for {
+    // infinite loop so we can recover from errors
+    for {
 
-		db, err := internaldb.CreateDbPoolConnection(ctx, log)
-		if err != nil {
-			log.Error().Err(err).Msg("failed to connect to database")
-			return
-		}
+        db, err := internaldb.CreateDbPoolConnection(ctx, log)
+        if err != nil {
+            log.Error().Err(err).Msg("failed to connect to database")
+            return
+        }
 
-		runMonopolyEngine(log, &engine, db)
+        runMonopolyEngine(log, &engine, db)
 
-	}
+    }
 }
 
 func runMonopolyEngine(log zerolog.Logger, e *MonopolyEngine, db *pgxpool.Pool) {
-	log.Info().Msgf("started monopoly engine for session id: %v", e.sessionId)
+    log.Info().Msgf("started monopoly engine for session id: %v", e.sessionId)
 
-	for {
+    for {
 
-		action := <-e.userActionsChan
-		log.Trace().Msgf("got action event: %v with data: %v", action.Event, action.Data)
+        action := <-e.userActionsChan
+        log.Trace().Msgf("got action event: %v with data: %v", action.Event, action.Data)
 
-	}
+    }
 
 }
