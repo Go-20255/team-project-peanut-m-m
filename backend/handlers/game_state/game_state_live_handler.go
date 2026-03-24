819e54a6757e6eaf4e75e6ab23d073b584e54cb9
Michael Brenner <mab7470@rit.edu>
2026-03-24 12:19:35 -0400

    join live game api setup complete, basic engine made, db functoin to get game sessions made

---
+++ backend/handlers/game_state/game_state_live_handler.go
@@ -0,0 +1,76 @@
+package game_state_handlers
+
+import (
+	"fmt"
+	"monopoly-backend/handlers"
+	monopoly_engine "monopoly-backend/internal/engine"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+
+
+
+func JoinLiveGameHandler(c echo.Context) error {
+
+    session_id := c.QueryParam("session_id")
+
+    player_id := c.FormValue("player_id")
+    player_name := c.FormValue("player_name")
+
+
+    w := c.Response().Writer
+    handlers.PrepareSseHeaders(w)
+
+    client := &handlers.SseClient{
+        ID:      fmt.Sprintf("%v-%v", player_name, player_id),
+        MsgChan: make(chan handlers.SseBroadcastMessage, 32),
+    }
+
+    broker, err := monopoly_engine.GetEngineBroker(session_id)
+    if err != nil {
+        return c.String(http.StatusBadRequest, "no monopoly engine running for session_id")
+    }
+
+    broker.AddClient(client)
+    defer broker.RemoveClient(client)
+
+    if err := handlers.WriteSseComment(w, "connected"); err != nil {
+        return err
+    }
+
+    // keep alive heart beat for connection
+    heartbeat := time.NewTicker(25 * time.Second)
+    defer heartbeat.Stop()
+
+    ctx := c.Request().Context()
+
+    if err := monopoly_engine.NotifyEngineOfAction(session_id, handlers.SseEvent{
+        Event: []byte("ConnectionEvent"),
+        Data: []byte(fmt.Sprintf("%v joined game", player_name)),
+    }); err != nil {
+        return c.String(http.StatusInternalServerError, err.Error())
+    }
+
+    for {
+        select {
+        case <-ctx.Done():
+            return nil
+        case msg, ok := <-client.MsgChan: // monitor for messages we need to broadcast to user
+            if !ok {
+                return nil
+            }
+
+            if err := handlers.WriteSseEvent(w, msg.EventName, msg.MsgObj); err != nil {
+                return err
+            }
+        case <-heartbeat.C:
+            if err := handlers.WriteSseComment(w, "keepalive"); err != nil {
+                return nil
+            }
+        }
+    }
+
+}
