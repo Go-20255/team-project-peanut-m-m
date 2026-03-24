a2bd583b47327910057e37351bc402f5db3486ec
Michael Brenner <mab7470@rit.edu>
2026-03-24 09:32:24 -0400

    retabbed

--- backend/handlers/game_state/game_state_handlers.go
+++ backend/handlers/game_state/game_state_handlers.go
@@ -1,28 +1,28 @@
 package game_state
 
 import (
-	internaldbgamestate "monopoly-backend/internal/db/game_state"
-	"monopoly-backend/util"
-	"net/http"
+    internaldbgamestate "monopoly-backend/internal/db/game_state"
+    "monopoly-backend/util"
+    "net/http"
 
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/labstack/echo/v4"
+    "github.com/jackc/pgx/v5/pgxpool"
+    "github.com/labstack/echo/v4"
 )
 
 func JoinGameHandler(c echo.Context) error {
-	log := util.GetRequestLogger(c)
-	ctx := c.Request().Context()
+    log := util.GetRequestLogger(c)
+    ctx := c.Request().Context()
 
-	code := c.QueryParam("code")
-	if code == "" {
-		return c.String(http.StatusBadRequest, "missing code")
-	}
+    code := c.QueryParam("code")
+    if code == "" {
+        return c.String(http.StatusBadRequest, "missing code")
+    }
 
-	tx := c.Get("tx").(*pgxpool.Tx)
-	id, err := internaldbgamestate.CheckGameStateCode(log, ctx, tx, code)
-	if err != nil {
-		return c.String(http.StatusBadRequest, "code does not exist")
-	}
+    tx := c.Get("tx").(*pgxpool.Tx)
+    id, err := internaldbgamestate.CheckGameStateCode(log, ctx, tx, code)
+    if err != nil {
+        return c.String(http.StatusBadRequest, "code does not exist")
+    }
 
-	return c.String(http.StatusOK, "joined game: "+id)
+    return c.String(http.StatusOK, "joined game: "+id)
 }
