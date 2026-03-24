821f0b40325694d3feae72f7b291017d69c85b99
Michael Brenner <mab7470@rit.edu>
2026-03-24 09:31:48 -0400

    retabbed

--- backend/main.go
+++ backend/main.go
@@ -1,72 +1,72 @@
 package main
 
 import (
-	"context"
-	commonhandler "monopoly-backend/handlers/common"
-	gamestatehandlers "monopoly-backend/handlers/game_state"
-	playershandlers "monopoly-backend/handlers/player"
-	internaldb "monopoly-backend/internal/db"
-	"monopoly-backend/util"
+    "context"
+    commonhandler "monopoly-backend/handlers/common"
+    gamestatehandlers "monopoly-backend/handlers/game_state"
+    playershandlers "monopoly-backend/handlers/player"
+    internaldb "monopoly-backend/internal/db"
+    "monopoly-backend/util"
 
-	"github.com/joho/godotenv"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"github.com/rs/zerolog/log"
+    "github.com/joho/godotenv"
+    "github.com/labstack/echo/v4"
+    "github.com/labstack/echo/v4/middleware"
+    "github.com/rs/zerolog/log"
 )
 
 func main() {
-	// create context to be used for lifetime of server
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+    // create context to be used for lifetime of server
+    ctx, cancel := context.WithCancel(context.Background())
+    defer cancel()
 
-	util.Setup_logging()
-	log := log.Logger.With().Logger()
+    util.Setup_logging()
+    log := log.Logger.With().Logger()
 
-	log.Info().Msg("Welcome To Monopoly")
-	err := godotenv.Load("../.internal.env")
-	if err != nil {
-		log.Info().Msg("WARNING: Failed to load .internal.env file in repo root")
-	}
+    log.Info().Msg("Welcome To Monopoly")
+    err := godotenv.Load("../.internal.env")
+    if err != nil {
+        log.Info().Msg("WARNING: Failed to load .internal.env file in repo root")
+    }
 
-	internaldb.SetupDatabase(ctx, log)
+    internaldb.SetupDatabase(ctx, log)
 
-	db, err := internaldb.CreateDbPoolConnection(ctx, log)
-	if err != nil {
-		log.Panic().Err(err).Msg("failed to connect to database")
-		return
-	}
+    db, err := internaldb.CreateDbPoolConnection(ctx, log)
+    if err != nil {
+        log.Panic().Err(err).Msg("failed to connect to database")
+        return
+    }
 
-	e := echo.New()
+    e := echo.New()
 
-	// allow us to use a custom logger for each api call
-	e.Use(util.RequestLoggerMiddleware)
-	// attach a database transaction to each api call
-	e.Use(internaldb.TxMiddleware(db))
+    // allow us to use a custom logger for each api call
+    e.Use(util.RequestLoggerMiddleware)
+    // attach a database transaction to each api call
+    e.Use(internaldb.TxMiddleware(db))
 
-	// setup cors
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{
-			"http://localhost:3001",
-			"http://localhost:3000",
-			"http://127.0.0.1:3001",
-			"http://127.0.0.1:3000",
-		},
-		AllowMethods:     []string{"GET", "POST", "DELETE", "PUT", "PATCH"},
-		AllowHeaders:     []string{"Authorization", "Content-Type"},
-		AllowCredentials: true,
-		ExposeHeaders:    []string{"Sec-WebSocket-Accept", "Sec-WebSocket-Protocol"},
-	}))
-	e.Use(middleware.Gzip())
+    // setup cors
+    e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+        AllowOrigins: []string{
+            "http://localhost:3001",
+            "http://localhost:3000",
+            "http://127.0.0.1:3001",
+            "http://127.0.0.1:3000",
+        },
+        AllowMethods:     []string{"GET", "POST", "DELETE", "PUT", "PATCH"},
+        AllowHeaders:     []string{"Authorization", "Content-Type"},
+        AllowCredentials: true,
+        ExposeHeaders:    []string{"Sec-WebSocket-Accept", "Sec-WebSocket-Protocol"},
+    }))
+    e.Use(middleware.Gzip())
 
-	routes := e.Group("/api")
+    routes := e.Group("/api")
 
-	// add routes here
-	routes.GET("/health", commonhandler.HealthCheckHandler)
+    // add routes here
+    routes.GET("/health", commonhandler.HealthCheckHandler)
 
-	routes.POST("/player", playershandlers.CreatePlayerHandler)
+    routes.POST("/player", playershandlers.CreatePlayerHandler)
 
-	routes.GET("/game/join", gamestatehandlers.JoinGameHandler)
+    routes.GET("/game/join", gamestatehandlers.JoinGameHandler)
 
-	// start the echo server
-	e.Start(":9876")
+    // start the echo server
+    e.Start(":9876")
 }
