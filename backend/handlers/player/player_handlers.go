337223a58136fb58888b6cb9698c9a36a8523cf4
0xmmalik <manavmalik36@gmail.com>
2026-04-26 21:31:31 -0400

    added player join handler, this now emits JWT instead of create player

--- backend/handlers/player/player_handlers.go
+++ backend/handlers/player/player_handlers.go
@@ -5,6 +5,7 @@ import (
     internaldbplayers "monopoly-backend/internal/db/player"
     "monopoly-backend/util"
     "net/http"
+    "strconv"
 
     "github.com/jackc/pgx/v5/pgxpool"
     "github.com/labstack/echo/v4"
@@ -39,13 +40,63 @@ func CreatePlayerHandler(c echo.Context) error {
         return c.String(http.StatusInternalServerError, "failed to create player in db")
     }
 
-    jwt, err := util.CreatePlayerJwt(id, name, sessionId)
+    return c.JSON(http.StatusOK, map[string]interface{}{
+        "id":         id,
+        "name":       name,
+        "session_id": sessionId,
+    })
+}
+
+func JoinPlayerHandler(c echo.Context) error {
+    log := util.GetRequestLogger(c)
+    ctx := c.Request().Context()
+
+    playerId := c.FormValue("player_id")
+    if playerId == "" {
+        return c.String(http.StatusBadRequest, "missing player_id")
+    }
+
+    name := c.FormValue("player_name")
+    if name == "" {
+        return c.String(http.StatusBadRequest, "missing player_name")
+    }
+
+    sessionId := c.FormValue("session_id")
+    if sessionId == "" {
+        return c.String(http.StatusBadRequest, "missing session_id")
+    }
+
+    tx := c.Get("tx").(*pgxpool.Tx)
+    exists, err := internaldbgamestate.GameStateExists(log, ctx, tx, sessionId)
+    if err != nil {
+        return c.String(http.StatusInternalServerError, "failed to query db about game state")
+    }
+
+    if !exists {
+        return c.String(http.StatusBadRequest, "session_id does not exist")
+    }
+
+    playerExists, err := internaldbplayers.CheckPlayerExists(log, ctx, tx, playerId, name, sessionId)
+    if err != nil {
+        return c.String(http.StatusInternalServerError, "failed to query db about player")
+    }
+
+    if !playerExists {
+        return c.String(http.StatusBadRequest, "player does not exist")
+    }
+
+    playerIdInt, err := strconv.Atoi(playerId)
+    if err != nil {
+        return c.String(http.StatusBadRequest, "player_id must be a valid integer")
+    }
+
+    jwt, err := util.CreatePlayerJwt(playerIdInt, name, sessionId)
     if err != nil {
         return c.String(http.StatusInternalServerError, "failed to create player auth token")
     }
 
     return c.JSON(http.StatusOK, map[string]interface{}{
-        "id":         id,
+        "id":         playerIdInt,
         "name":       name,
         "session_id": sessionId,
         "jwt":        jwt,
